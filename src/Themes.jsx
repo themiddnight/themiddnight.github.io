1310c5f44c3c2b4d0b53f619a3f5e5f454c2d3bb
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-02-20 22:06:33 +0700

    add random mesh gradient background

--- src/Themes.jsx
+++ src/Themes.jsx
@@ -10,63 +10,108 @@ import "./App.css";
 
 export default function Themes({ children }) {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const memoTheme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "dark" : "light",
-          background: {
-            default: prefersDarkMode ? "#121212" : "#e8e8e8",
+
+  const memoTheme = useMemo(() => {
+    const darkBg = "#1d1d1d";
+    const lightBg = "#dddddd";
+
+    // Generate random number between min and max
+    function rand(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+    // Generate random mesh style for body background
+    function generateMeshStyle() {
+      let meshStyleTx = ``;
+      for (let i = 0; i < rand(4, 6); i++) {
+        let randMeshStyle_txt = `,
+          radial-gradient(
+              at ${rand(0, 100)}% ${rand(0, 100)}%, 
+              hsl(
+                ${rand(0, 360)}, 
+                ${rand(40, 80)}%, 
+                ${rand(
+                  prefersDarkMode ? 30 : 50, 
+                  prefersDarkMode ? 50 : 80
+                )}%
+              ) 0%, 
+              transparent ${rand(50, 90)}%)`;
+        meshStyleTx += randMeshStyle_txt;
+      }
+      return meshStyleTx;
+    }
+
+    return createTheme({
+      palette: {
+        mode: prefersDarkMode ? "dark" : "light",
+        background: {
+          default: prefersDarkMode ? darkBg : lightBg,
+        },
+      },
+      shape: {
+        borderRadius: 20,
+      },
+      components: {
+        MuiCssBaseline: {
+          styleOverrides: {
+            body: {
+              backgroundSize: "100% 100dvh",
+              backgroundRepeat: "no-repeat",
+              backgroundAttachment: "fixed",
+              backgroundImage: `
+                linear-gradient(
+                  to bottom, transparent 20%, 
+                  ${prefersDarkMode ? darkBg : lightBg} 95%
+                ) 
+                ${generateMeshStyle()}`,
+            },
           },
         },
-        shape: {
-          borderRadius: 20,
-        },
-        components: {
-          MuiListItemIcon: {
-            styleOverrides: {
-              root: {
-                minWidth: 35,
-              },
-            },
-          },
-          MuiCard: {
-            styleOverrides: {
-              root: {
-                padding: 10,
-                paddingBottom: 3,
-                boxShadow: "0 3px 8px 0 rgba(0, 0, 0, 0.2)",
-                borderTop: "1px solid #ffffff11",
-                borderInline: "1px solid #00000022",
-                borderBottom: "1px solid #00000044",
-                backgroundImage: prefersDarkMode
-                  ? "linear-gradient(to bottom, #202020, #282828)"
-                  : "linear-gradient(to bottom, #f4f4f4, #ffffff)",
-              },
-            },
-          },
-          MuiListItem: {
-            styleOverrides: {
-              root: {
-                paddingTop: 1,
-                paddingBottom: 1,
-              },
-            },
-          },
-          MuiTooltip: {
-            styleOverrides: {
-              tooltipArrow: {
-                padding: 8,
-                borderRadius: 5,
-                filter: "drop-shadow(0 3px 5px rgba(0,0,0,0.5))",
-                backdropFilter: "blur(5px)",
-              },
+        MuiListItemIcon: {
+          styleOverrides: {
+            root: {
+              minWidth: 35,
             },
           },
         },
-      }),
-    [prefersDarkMode]
-  );
+        MuiCard: {
+          styleOverrides: {
+            root: {
+              padding: 10,
+              paddingBottom: 3,
+              boxShadow: "0 3px 8px 0 rgba(0, 0, 0, 0.2), inset 0 0 50px 0 rgba(0, 0, 0, 0.05)",
+              backgroundColor: prefersDarkMode ? "#000000cc" : "#f4f4f4dd",
+              // boxShadow: "0 3px 8px 0 rgba(0, 0, 0, 0.2)",
+              // borderTop: "1px solid #ffffff11",
+              // borderInline: "1px solid #00000022",
+              // borderBottom: "1px solid #00000044",
+              // backgroundImage: prefersDarkMode
+              //   ? "linear-gradient(to bottom, #101010, #181818)"
+              //   : "linear-gradient(to bottom, #f4f4f4, #ffffff)",
+            },
+          },
+        },
+        MuiListItem: {
+          styleOverrides: {
+            root: {
+              paddingTop: 1,
+              paddingBottom: 1,
+            },
+          },
+        },
+        MuiTooltip: {
+          styleOverrides: {
+            tooltipArrow: {
+              padding: 8,
+              borderRadius: 5,
+              filter: "drop-shadow(0 3px 5px #00000055)",
+              backdropFilter: "blur(10px)",
+              backgroundColor: '#666666bb'
+            },
+          },
+        },
+      },
+    });
+  }, [prefersDarkMode]);
 
   return (
     <ThemeProvider theme={memoTheme}>
