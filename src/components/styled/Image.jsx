2a324ce75c179bf6535f358927331e00ed97df16
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-03-03 03:12:55 +0700

    Add styled Image component

---
+++ src/components/styled/Image.jsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/system";
+
+export const Image = styled("img")(({ zoomed }) => ({
+  objectFit: "cover",
+  transition: "transform 0.3s",
+  "&:hover": {
+    transform: zoomed ? "scale(1.2)" : "none",
+  },
+}));
