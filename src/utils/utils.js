4bf287949db769a95539fc6635ab6b43ed64658c
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-03-21 16:49:15 +0700

    Add getRelativeTime function to utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -21,3 +21,35 @@ export function sortByDate(data, attr = "to", desc = true) {
     }
   });
 }
+
+export function getRelativeTime(timestamp) {
+  const date = new Date(timestamp._seconds * 1000);
+  const now = new Date();
+  const diff = now - date;
+
+  if (diff < 1000 * 60) {
+    return "Just now";
+  } else if (diff < 1000 * 60 * 60) {
+    const min = Math.floor(diff / (1000 * 60));
+    return `${min} ${min === 1 ? "min." : "mins."}`;
+  } else if (diff < 1000 * 60 * 60 * 24) {
+    const hour = Math.floor(diff / (1000 * 60 * 60));
+    return `${hour} ${hour === 1 ? "hr." : "hrs."}`;
+  } else if (diff < 1000 * 60 * 60 * 24 * 7) {
+    const day = Math.floor(diff / (1000 * 60 * 60 * 24));
+    return `${day} ${day === 1 ? "day" : "days"}`;
+  } else if (diff < 1000 * 60 * 60 * 24 * 30) {
+    const week = Math.floor(diff / (1000 * 60 * 60 * 24 * 7));
+    return `${week} ${week === 1 ? "week" : "weeks"}`;
+  } else if (diff < 1000 * 60 * 60 * 24 * 365) {
+    return date.toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+    });
+  } else {
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+    });
+  }
+}
