ccfd57f614ee8ec7aa0bca53d47db4dfc73728fc
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-03-21 16:50:33 +0700

    Refactor image modal and add public notes components

---
+++ src/components/modals/ImageModal.jsx
@@ -0,0 +1,38 @@
+import { Box, Modal, Fade } from "@mui/material";
+import { useContext } from "react";
+import { ModalContext } from "../../App";
+
+export default function ImageModal() {
+  const { isImageModalOpen, setIsImageModalOpen, imageModalSrc } = useContext(ModalContext);
+  
+  return (
+    <Modal
+      open={isImageModalOpen}
+      onClose={() => setIsImageModalOpen(false)}
+      aria-labelledby="image-modal"
+      aria-describedby="image-modal"
+      disableAutoFocus
+      disableRestoreFocus
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Fade in={isImageModalOpen}>
+        <Box
+          width={{ sm: "100%", md: "80%", lg: "70%", xl: "60%" }}
+          borderRadius={{ sm: 0, md: 1 }}
+          overflow={"hidden"}
+          boxShadow={10}
+          position={'absolute'}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <img src={imageModalSrc} alt="modal" width={"100%"} height={"100%"} />
+        </Box>
+      </Fade>
+    </Modal>
+  );
+}
