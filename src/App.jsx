476d4588f3fac44188cfccdf1a320eb1c13cd0df
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-04-08 23:53:14 +0700

    Update App.jsx to use react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import { useState, createContext } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-import Themes from "./Themes";
-import Home from "./Home";
+import HomePage from "./pages/Home";
+import EditPage from "./pages/Edit";
 
 export const ModalContext = createContext();
 
 export default function App() {
   const [isImageModalOpen, setIsImageModalOpen] = useState(false);
-  const [isPublicNotesModalOpen, setIsPublicNotesModalOpen] = useState(false);
   const [imageModalSrc, setImageModalSrc] = useState("");
+  const [authToken, setAuthToken] = useState("");
 
   return (
-    <Themes>
-      <ModalContext.Provider
-        value={{
-          isImageModalOpen,
-          setIsImageModalOpen,
-          isPublicNotesModalOpen,
-          setIsPublicNotesModalOpen,
-          imageModalSrc,
-          setImageModalSrc,
-        }}
-      >
-        <Home />
-      </ModalContext.Provider>
-    </Themes>
+    <ModalContext.Provider
+      value={{
+        isImageModalOpen,
+        setIsImageModalOpen,
+        imageModalSrc,
+        setImageModalSrc,
+        authToken,
+        setAuthToken,
+      }}
+    >
+      <HashRouter basename="/">
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/:resumeId" element={<HomePage />} />
+          <Route exact path="/create/:resumeId" element={<EditPage />} />
+        </Routes>
+      </HashRouter>
+    </ModalContext.Provider>
   );
 }
